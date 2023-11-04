ee379992c9027a07413e3779fbd1bafc5f235ecb
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2023-11-04 14:51:05 +0300

    install.py

---
+++ install.py
@@ -0,0 +1,26 @@
+import os
+import pyfiglet
+import random
+from termcolor import colored
+
+colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+
+selected_color = random.choice(colors)
+
+text = 'I N S T A L L'
+
+lo = pyfiglet.figlet_format(text)
+colored_lo = colored(lo, color=selected_color)
+
+print(colored_lo)
+
+libraries_to_install = [
+    "pyfiglet",
+    "termcolor",
+    "scapy",
+    "colored",
+    "netsh",
+]
+
+for library in libraries_to_install:
+    os.system(f"pip install {library}")
